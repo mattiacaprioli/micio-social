2d4c4b56fd728c4b2b4b0c8d76452d1229edb259
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-11-21 19:13:22 +0100

    feat first page setup

---
+++ app/welcome.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../components/ScreenWrapper'
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Text>welcome</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({})
