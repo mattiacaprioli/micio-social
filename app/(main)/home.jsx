b8a03a015ec084d04ef3f7380eda5a30f9ca6acc
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-11-24 13:49:15 +0100

    start style home page, feat router for notifications, profile, newPost

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,12 +1,17 @@
-import { Alert, Button, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
 import { useAuth } from '../../context/AuthContext'
 import { supabase } from '../../lib/supabase'
+import { wp, hp } from '../../helpers/common'
+import { theme } from '../../constants/theme'
+import Icon from '../../assets/icons'
+import { useRouter } from 'expo-router'
 
 const Home = () => {
 
   const {user, setAuth} = useAuth();
+  const router = useRouter();
 
   console.log('user: ', user);
 
@@ -20,7 +25,23 @@ const Home = () => {
 
   return (
     <ScreenWrapper>
-      <Text>Home</Text>
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>LinkUp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push('notifications')}>
+              <Icon name="heart" size={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+            <Pressable onPress={() => router.push('newPost')}>
+              <Icon name="plus" size={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+            <Pressable onPress={() => router.push('profile')}>
+              <Icon name="user" size={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+          </View>
+        </View>
+      </View>
       <Button title="Logout" onPress={onLogout} />
     </ScreenWrapper>
   )
@@ -28,4 +49,60 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // paddingHorizontal: wr(4),
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    width: hp(4.3),
+    height: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: 'continuous',
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+  noPost: {
+    fontSize: hp(2),
+    textAlign: 'center',
+    color: theme.colors.text,
+  },
+  pill: {
+    position: 'absolute',
+    right: -10,
+    top: -4,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: theme.colors.roseLight,
+  },
+  pillText: {
+    color: 'white',
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold,
+  },
+})
