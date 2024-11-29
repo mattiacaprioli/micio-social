f1fdd05c0a29b1803ba115e71a6e8df677d7bb01
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-11-29 18:53:49 +0100

    feat like call

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,21 +1,21 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { theme } from '../constants/theme';
-import { hp, wp } from '../helpers/common';
-import Avatar from './Avatar';
-import moment from 'moment';
-import Icon from '../assets/icons';
-import RenderHtml from 'react-native-render-html';
-import { color } from '@rneui/themed/dist/config';
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { theme } from "../constants/theme";
+import { hp, wp } from "../helpers/common";
+import Avatar from "./Avatar";
+import moment from "moment";
+import Icon from "../assets/icons";
+import RenderHtml from "react-native-render-html";
+import { color } from "@rneui/themed/dist/config";
 import { Image } from "expo-image";
-import { getSupabaseFileUrl } from '../services/imageService';
-import { Video } from 'expo-av';
+import { getSupabaseFileUrl } from "../services/imageService";
+import { Video } from "expo-av";
+import { createPostLike, removePostLike } from "../services/postService";
 
 const textStyles = {
   color: theme.colors.dark,
   fontSize: hp(1.75),
-}
-
+};
 
 const tagsStyles = {
   div: textStyles,
@@ -27,14 +27,9 @@ const tagsStyles = {
   h4: {
     color: theme.colors.dark,
   },
-}
+};
 
-const PostCard = ({
-  item,
-  currentUser,
-  router,
-  hasShadow = true,
-}) => {
+const PostCard = ({ item, currentUser, router, hasShadow = true }) => {
   const shadowStyle = {
     shadowOffset: {
       width: 0,
@@ -43,15 +38,49 @@ const PostCard = ({
     shadowOpacity: 0.05,
     shadowRadius: 6,
     elevation: 1,
-  }
+  };
+
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    setLikes(item?.postLikes);
+  }, []);
 
   const openPostDetails = () => {
     // later
-  }
+  };
 
-  const createdAt = moment(item?.created_at).format('MMM D');
-  const likes = [];
-  const liked = false;
+  const onLike = async () => {
+    if (liked) {
+      // remove like
+      let updatedLikes = likes.filter((like) => like.userId != currentUser?.id);
+      setLikes([...updatedLikes]);
+      let res = await removePostLike(item?.id, currentUser?.id);
+
+      console.log("removed like: ", res);
+      if (!res.success) {
+        Alert.alert("Post", "Something went wrong!");
+      }
+    } else {
+      // create like
+      let data = {
+        userId: currentUser?.id,
+        postId: item?.id,
+      };
+      setLikes([...likes, data]);
+      let res = await createPostLike(data);
+
+      console.log("added like: ", res);
+      if (!res.success) {
+        Alert.alert("Post", "Something went wrong!");
+      }
+    }
+  };
+
+  const createdAt = moment(item?.created_at).format("MMM D");
+  const liked = likes.filter((like) => like.userId == currentUser?.id)[0]
+    ? true
+    : false;
 
   return (
     <View style={[styles.container, hasShadow && shadowStyle]}>
@@ -62,84 +91,71 @@ const PostCard = ({
             uri={item?.user?.image}
             rounded={theme.radius.xl}
           />
-          <View style={{gap: 2}}>
+          <View style={{ gap: 2 }}>
             <Text style={styles.username}>{item?.user?.name}</Text>
             <Text style={styles.postTime}>{createdAt}</Text>
           </View>
         </View>
         <TouchableOpacity onPress={openPostDetails}>
-          <Icon name="threeDotsHorizontal" size={hp(3.4)} color={theme.colors.text} />
+          <Icon
+            name="threeDotsHorizontal"
+            size={hp(3.4)}
+            color={theme.colors.text}
+          />
         </TouchableOpacity>
       </View>
 
       {/* post body & video */}
       <View style={styles.content}>
         <View style={styles.postBody}>
-          {
-            item?.body && (
-              <RenderHtml
-                source={{html: item?.body}}
-                contentWidth={wp(100)}
-                tagsStyles={tagsStyles}
-              />
-            )
-          }
+          {item?.body && (
+            <RenderHtml
+              source={{ html: item?.body }}
+              contentWidth={wp(100)}
+              tagsStyles={tagsStyles}
+            />
+          )}
         </View>
 
         {/* post image */}
-        {
-          item?.file && item?.file?.includes('postImages') && (
-            <Image
-              source={getSupabaseFileUrl(item?.file)}
-              transitionDuration={100}
-              style={styles.postMedia}
-              contentFit='cover'
-            />
-          )
-        }
+        {item?.file && item?.file?.includes("postImages") && (
+          <Image
+            source={getSupabaseFileUrl(item?.file)}
+            transitionDuration={100}
+            style={styles.postMedia}
+            contentFit="cover"
+          />
+        )}
 
         {/* post image */}
-        {
-          item?.file && item?.file?.includes('postVideos') && (
-            <Video
-              source={getSupabaseFileUrl(item?.file)}
-              transitionDuration={100}
-              style={[styles.postMedia, {height: hp(30)}]}
-              useNativeControls
-              resizeMode='cover'
-              isLooping
-            />
-          )
-        }
+        {item?.file && item?.file?.includes("postVideos") && (
+          <Video
+            source={getSupabaseFileUrl(item?.file)}
+            transitionDuration={100}
+            style={[styles.postMedia, { height: hp(30) }]}
+            useNativeControls
+            resizeMode="cover"
+            isLooping
+          />
+        )}
 
-        
         {/* like, comment & share */}
         <View style={styles.footer}>
           <View style={styles.footerButton}>
-            <TouchableOpacity>
-              {
-                liked ? (
-                  <Icon name="heartFill" size={24} color={theme.colors.rose} />
-                ) : (
-                  <Icon name="heart" size={24} color={theme.colors.textLight} />
-                )
-              }
+            <TouchableOpacity onPress={onLike}>
+              {liked ? (
+                <Icon name="heartFill" size={24} color={theme.colors.rose} />
+              ) : (
+                <Icon name="heart" size={24} color={theme.colors.textLight} />
+              )}
             </TouchableOpacity>
-            <Text style={styles.count}>
-              {
-                likes?.length
-              }
-            </Text>
+            <Text style={styles.count}>{likes?.length}</Text>
           </View>
           <View style={styles.footerButton}>
             <TouchableOpacity>
               <Icon name="comment" size={24} color={theme.colors.textLight} />
             </TouchableOpacity>
-            <Text style={styles.count}>
-              {
-                0
-              }
-            </Text>
+            <Text style={styles.count}>{0}</Text>
           </View>
           <View style={styles.footerButton}>
             <TouchableOpacity>
@@ -149,31 +165,31 @@ const PostCard = ({
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
 
 const styles = StyleSheet.create({
   container: {
     gap: 10,
     marginBottom: 15,
-    borderRadius: theme.radius.xxl*1.1,
-    borderCurve: 'continuous',
+    borderRadius: theme.radius.xxl * 1.1,
+    borderCurve: "continuous",
     padding: 10,
     paddingVertical: 12,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 0.5,
     borderColor: theme.colors.gray,
-    shadowColor: '#000',
+    shadowColor: "#000",
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   userInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   username: {
@@ -192,31 +208,31 @@ const styles = StyleSheet.create({
   },
   postMedia: {
     height: hp(40),
-    width: '100%',
+    width: "100%",
     borderRadius: theme.radius.xl,
-    borderCurve: 'continuous',
+    borderCurve: "continuous",
   },
   postBody: {
     marginLeft: 5,
   },
   footer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 15,
   },
   footerButton: {
     marginLeft: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 4,
   },
   actionw: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 18,
   },
   count: {
     color: theme.colors.text,
     fontSize: hp(1.8),
-  }
-})
+  },
+});
